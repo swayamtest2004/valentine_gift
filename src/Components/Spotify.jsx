3f01cd9698191cec5b4fd52cc863fb6a323c1ac2
swayam <swayam@techysquad.com>
2026-02-13 18:23:12 +0530

    added buttons

--- src/Components/Spotify.jsx
+++ src/Components/Spotify.jsx
@@ -1,5 +1,6 @@
 import { useRef, useState, useEffect } from "react";
 import { motion } from "framer-motion";
+import { Link } from "react-router-dom";
 
 const Spotify = () => {
     const audioRef = useRef(null);
